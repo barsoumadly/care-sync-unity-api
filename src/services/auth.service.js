503229dd214b806e544436453a97f991fdd4bab7
Kerolos <kerolos.saaid@gmail.com>
2024-12-13 18:44:24 +0200

    implemented login

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,6 +1,12 @@
-const login = async ({ email!, password }) => {
-    // TODO: Implement login
+const { StatusCodes } = require("http-status-codes");
+const User = require("../models/User");
+
+const login = async ({ email, password }) => {
+  const user = await User.findOne({ email });
+  if (!user || !(await user.isPasswordMatch(password))) {
+    throw new ApiError(StatusCodes.UNAUTHORIZED, "Incorrect email or password");
+  }
+  return user;
 };
 
 module.exports = { login };
-
