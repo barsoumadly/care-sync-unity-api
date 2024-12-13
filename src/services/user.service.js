5a2992b1d1f045f465422a3ae2cffd019217d8e9
Kerolos <kerolos.saaid@gmail.com>
2024-12-13 05:27:15 +0200

    implemented createUser

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,5 +1,17 @@
+const User = require("../models/User");
+const ApiError = require("../utils/ApiError");
+const { StatusCodes } = require("http-status-codes");
+
+/**
+ * Create a user
+ * @param {Object} userBody
+ * @returns {Promise<User>}
+ */
 const createUser = async (user) => {
-  // TODO: Implement createUser
+  if (await User.isEmailTaken(user.email)) {
+    throw new ApiError(StatusCodes.BAD_REQUEST, "Email already taken");
+  }
+  return User.create(user);
 };
 
 module.exports = { createUser };
