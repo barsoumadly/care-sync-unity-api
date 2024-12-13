a9b2e925e2bb53489eb7a3e987594a2243ce6f54
Kerolos <kerolos.saaid@gmail.com>
2024-12-13 03:34:13 +0200

    added hash salt

--- src/config/envVariables.js
+++ src/config/envVariables.js
@@ -6,6 +6,7 @@ const envVariables = {
   NODE_ENV: process.env.NODE_ENV,
   PORT: process.env.PORT || 8000,
   MONGO_URI: process.env.MONGO_URI,
+  HASH_SALT: process.env.HASH_SALT || 8,
 };
 
 module.exports = envVariables;
