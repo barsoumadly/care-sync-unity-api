1c93866be5b70456b5d5f5c2c665c57a64885496
Kerolos <kerolos.saaid@gmail.com>
2024-12-13 19:50:26 +0200

    import ApiError & fixed switched params

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,10 +1,11 @@
 const { StatusCodes } = require("http-status-codes");
 const User = require("../models/User");
+const ApiError = require("../utils/ApiError");
 
 const login = async ({ email, password }) => {
   const user = await User.findOne({ email });
   if (!user || !(await user.isPasswordMatch(password))) {
-    throw new ApiError(StatusCodes.UNAUTHORIZED, "Incorrect email or password");
+    throw new ApiError("Incorrect email or password", StatusCodes.UNAUTHORIZED);
   }
   return user;
 };
