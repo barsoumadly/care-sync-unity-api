1ee15e93c9ee60a268c6fe67f2edc7c42b53c4c9
Kerolos <kerolos.saaid@gmail.com>
2024-12-13 05:39:34 +0200

    removed misplaced method

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -2,9 +2,5 @@ const login = async ({ email!, password }) => {
     // TODO: Implement login
 };
 
-const register = async ({ name, email, password, role }) => {
-    // TODO: Implement register
-};
-
-module.exports = { login, register };
+module.exports = { login };
 
