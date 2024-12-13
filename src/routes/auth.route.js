72c1d2339fd300f2e978c52da334e64aabd71e7a
Kerolos <kerolos.saaid@gmail.com>
2024-12-13 19:51:30 +0200

    corrected http method

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,8 +1,9 @@
+const authController = require("../controllers/auth.controller");
 const { Router } = require("express");
 
 const router = Router();
 
 router.post("/register", authController.register);
-router.post("/login", authController.login);
+router.get("/login", authController.login);
 
 module.exports = router;
