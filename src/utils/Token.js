17a0540ef59289c6fd685b247cdd0319b17da08f
Kerolos <kerolos.saaid@gmail.com>
2024-12-13 05:30:08 +0200

    init token utils

---
+++ src/utils/Token.js
@@ -0,0 +1,12 @@
+const createToken = (user) => {
+  // TODO: Implement createToken
+};
+
+const verifyToken = (token) => {
+  // TODO: Implement verifyToken
+};
+
+module.exports = {
+  createToken,
+  verifyToken,
+};
