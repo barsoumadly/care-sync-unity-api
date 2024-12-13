e5ad19944dcde674333eeb18873736af312f4f72
Kerolos <kerolos.saaid@gmail.com>
2024-12-13 20:03:09 +0200

    implemented auth controller

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,9 +1,20 @@
+const AsyncHandler = require("../utils/AsyncHandler");
+const userService = require("../services/user.service");
+const authServicve = require("../services/auth.service");
+const { StatusCodes } = require("http-status-codes");
+const tokenUtils = require("../utils/Token");
+
 const register = AsyncHandler(async (req, res) => {
-  // TODO: Implement register controller
+  await userService.createUser(req.body);
+  res
+    .json({ message: "User created successfully" })
+    .status(StatusCodes.CREATED);
 });
 
 const login = AsyncHandler(async (req, res) => {
-  // TODO: Implement login controller
+  const user = await authServicve.login(req.body);
+  const token = await tokenUtils.createToken(user);
+  res.json({ token }).status(StatusCodes.OK);
 });
 
 module.exports = { register, login };
