389173099a5d8e8f9d7a05da3ace59c1178ad98c
Kerolos <kerolos.saaid@gmail.com>
2024-12-13 19:51:17 +0200

    global error handler middleware

---
+++ src/middlewares/errorHandler.js
@@ -0,0 +1,10 @@
+const globalErrorHandler = (error, req, res, next) => {
+  const statusCode = error.statusCode || 500;
+  const status = error.status || "error";
+  res.status(statusCode).json({
+    status,
+    message: error.message,
+  });
+};
+
+module.exports = globalErrorHandler;
