875d6312936375623e5ddeaee2d2138ec84ff3ae
Kerolos <kerolos.saaid@gmail.com>
2024-12-13 03:33:48 +0200

    init user service

---
+++ src/services/user.service.js
@@ -0,0 +1,9 @@
+const hashPassword = async (password) => {
+  // TODO: Implement hashPassword
+};
+
+const comparePassword = async (password, hashedPassword) => {
+  // TODO: Implement comparePassword
+};
+
+module.exports = { hashPassword, comparePassword };
