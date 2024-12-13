2c847b2235dbc8d18145666902efa8098815d87e
Kerolos <kerolos.saaid@gmail.com>
2024-12-13 20:07:50 +0200

    convert hash salt to num

--- src/config/envVariables.js
+++ src/config/envVariables.js
@@ -6,7 +6,7 @@ const envVariables = {
   NODE_ENV: process.env.NODE_ENV,
   PORT: process.env.PORT || 8000,
   MONGO_URI: process.env.MONGO_URI,
-  HASH_SALT: process.env.HASH_SALT || 8,
+  HASH_SALT: +process.env.HASH_SALT || 8,
   TOKEN_SIGNATURE: process.env.TOKEN_SIGNATURE || "this is a default signature",
   TOKEN_EXPIRE_TIME: process.env.TOKEN_EXPIRE_TIME || "1m",
 };
