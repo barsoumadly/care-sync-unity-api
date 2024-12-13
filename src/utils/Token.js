6942952bcaad1f894a83844aa292275c5cdb3fef
Kerolos <kerolos.saaid@gmail.com>
2024-12-13 05:38:36 +0200

    implemented token utils

--- src/utils/Token.js
+++ src/utils/Token.js
@@ -1,5 +1,14 @@
+const jwt = require("jsonwebtoken");
+const {
+  TOKEN_SIGNATURE,
+  TOKEN_EXPIRE_TIME,
+} = require("../config/envVariables");
+
 const createToken = (user) => {
-  // TODO: Implement createToken
+  const token = jwt.sign({ id: user._id }, TOKEN_SIGNATURE, {
+    expiresIn: TOKEN_EXPIRE_TIME,
+  });
+  return token;
 };
 
 const verifyToken = (token) => {
