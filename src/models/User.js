45d8993386890bd05680b9beb1981b91c6b5a1a9
Kerolos <kerolos.saaid@gmail.com>
2024-12-13 05:42:43 +0200

    init user model

---
+++ src/models/User.js
@@ -0,0 +1,73 @@
+const mongoose = require("mongoose");
+const { roles } = require("../config/roles");
+const passwordUtils = require("../utils/Password");
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 8,
+    },
+    role: {
+      type: String,
+      enum: roles,
+      default: "patient",
+    },
+    isEmailVerified: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+/**
+ * Check if email is taken
+ * @param {string} email - The user's email
+ * @param {ObjectId} [excludeUserId] - The id of the user to be excluded
+ * @returns {Promise<boolean>}
+ */
+userSchema.statics.isEmailTaken = async function (email, excludeUserId) {
+  const user = await this.findOne({ email, _id: { $ne: excludeUserId } });
+  return !!user;
+};
+
+/**
+ * Check if password matches the user's password
+ * @param {string} password
+ * @returns {Promise<boolean>}
+ */
+userSchema.methods.isPasswordMatch = async function (password) {
+  const user = this;
+  return await passwordUtils.comparePassword(password, user.password);
+};
+
+userSchema.pre("save", async function (next) {
+  const user = this;
+  if (user.isModified("password")) {
+    user.password = await passwordUtils.hashPassword(user.password);
+  }
+  next();
+});
+
+/**
+ * @typedef User
+ */
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
