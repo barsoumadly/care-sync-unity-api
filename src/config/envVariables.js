c1605ac2e810d4bc404332b077967784aea417c9
Kerolos <kerolos.saaid@gmail.com>
2024-12-13 05:10:09 +0200

    added TOKEN_SIGNATURE

--- src/config/envVariables.js
+++ src/config/envVariables.js
@@ -7,6 +7,7 @@ const envVariables = {
   PORT: process.env.PORT || 8000,
   MONGO_URI: process.env.MONGO_URI,
   HASH_SALT: process.env.HASH_SALT || 8,
+  TOKEN_SIGNATURE: process.env.TOKEN_SIGNATURE || "this is a default signature",
 };
 
 module.exports = envVariables;
