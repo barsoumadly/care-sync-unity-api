f5e622664c7dd22752dd2ce81329c30ea4255db4
Kerolos <kerolos.saaid@gmail.com>
2024-12-13 03:33:30 +0200

    init auth service

---
+++ src/services/auth.service.js
@@ -0,0 +1,10 @@
+const login = async ({ email!, password }) => {
+    // TODO: Implement login
+};
+
+const register = async ({ name, email, password, role }) => {
+    // TODO: Implement register
+};
+
+module.exports = { login, register };
+
