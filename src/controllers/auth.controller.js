823b76023fdab1ead39d1f48212dfff22ff6faa5
Kerolos <kerolos.saaid@gmail.com>
2024-12-13 05:48:24 +0200

    init auth controller

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,9 @@
+const register = AsyncHandler(async (req, res) => {
+  // TODO: Implement register controller
+});
+
+const login = AsyncHandler(async (req, res) => {
+  // TODO: Implement login controller
+});
+
+module.exports = { register, login };
