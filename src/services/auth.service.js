141865dc5c36dfb733bd1b3ba2a6d43279b3405d
Kerolos <kerolos.saaid@gmail.com>
2024-12-16 01:38:35 +0200

    implemented verifyEmail

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,6 +1,7 @@
 const { StatusCodes } = require("http-status-codes");
 const User = require("../models/User");
 const ApiError = require("../utils/ApiError");
+const tokenService = require("./token.service");
 
 const login = async ({ email, password }) => {
   const user = await User.findOne({ email });
@@ -10,4 +11,13 @@ const login = async ({ email, password }) => {
   return user;
 };
 
-module.exports = { login };
+const verifyEmail = async (token) => {
+  const { userId } = await tokenService.decodeEmailVerificationToken(token);
+  const user = await User.findById(userId);
+  if (!user) {
+    throw new ApiError("User not found", StatusCodes.NOT_FOUND);
+  }
+  await user.verifyEmail(token);
+};
+
+module.exports = { login, verifyEmail };
