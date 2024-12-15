1845c5d83b788254c15081e772ef9d6b4b451188
Kerolos <kerolos.saaid@gmail.com>
2024-12-16 01:34:36 +0200

    index for email templates

---
+++ src/templates/email/index.js
@@ -0,0 +1,11 @@
+const passwordResetRequest = require("./passwordResetRequest");
+const passwordResetSuccess = require("./passwordResetSuccess");
+const emailVerificationRequest = require("./emailVerificationRequest");
+const emailVerificationSuccess = require("./emailVerificationSuccess");
+
+module.exports = {
+  passwordResetRequest,
+  passwordResetSuccess,
+  emailVerificationRequest,
+  emailVerificationSuccess,
+};
