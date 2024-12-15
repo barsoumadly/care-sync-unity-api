ca43dd97f28cb84529fb4237cd9a2fe3c460074d
Kerolos <kerolos.saaid@gmail.com>
2024-12-16 01:36:28 +0200

    sending email verification after register

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,19 +1,26 @@
 const AsyncHandler = require("../utils/AsyncHandler");
 const userService = require("../services/user.service");
-const authServicve = require("../services/auth.service");
+const authService = require("../services/auth.service");
 const { StatusCodes } = require("http-status-codes");
-const tokenUtils = require("../utils/Token");
+const emailService = require("../services/email.service");
+const tokenService = require("../services/token.service");
+const urlGenerator = require("../helpers/urlGenerator");
 
 const register = AsyncHandler(async (req, res) => {
-  await userService.createUser(req.body);
+  const newUser = await userService.createUser(req.body);
+  const token = await tokenService.generateEmailVerificationToken({
+    userId: newUser._id,
+  });
+  verificationLink = urlGenerator(req, `verify-email?token=${token}`);
+  emailService.sendEmailVerificationRequest(newUser.email, verificationLink);
   res
     .json({ message: "User created successfully" })
     .status(StatusCodes.CREATED);
 });
 
 const login = AsyncHandler(async (req, res) => {
-  const user = await authServicve.login(req.body);
-  const token = await tokenUtils.createToken(user);
+  const user = await authService.login(req.body);
+  const token = await tokenService.generateAuthToken({ userId: user.id });
   res.json({ token }).status(StatusCodes.OK);
 });
 
