5802eadabaae2a1ef365da786baf4ade362debc3
Kerolos <kerolos.saaid@gmail.com>
2024-12-16 01:37:55 +0200

    implemented get user by id

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -14,4 +14,13 @@ const createUser = async (user) => {
   return User.create(user);
 };
 
-module.exports = { createUser };
+/**
+ * Get user by id
+ * @param {ObjectId} id
+ * @returns {Promise<User>}
+ */
+const getUserById = async (id) => {
+  return User.findById(id);
+};
+
+module.exports = { createUser, getUserById };
