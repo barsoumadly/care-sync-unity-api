d13ebfbe2d89510edb8b18f80b05dd89fc7c6985
Kerolos <kerolos.saaid@gmail.com>
2024-12-16 01:36:50 +0200

    added email & token variables

--- src/config/envVariables.js
+++ src/config/envVariables.js
@@ -7,13 +7,21 @@ const envVariables = {
   PORT: process.env.PORT || 8000,
   MONGO_URI: process.env.MONGO_URI,
   HASH_SALT: +process.env.HASH_SALT || 8,
-  TOKEN_SIGNATURE: process.env.TOKEN_SIGNATURE || "this is a default signature",
-  TOKEN_EXPIRE_TIME: process.env.TOKEN_EXPIRE_TIME || "1m",
+  tokenConfig: {
+    AUTH_SIGNATURE: process.env.AUTH_TOKEN_SIGNATURE || "signature",
+    REFRESH_SIGNATURE: process.env.REFRESH_TOKEN_SIGNATURE || "signature",
+    EMAIL_VERIFICATION_SIGNATURE:
+      process.env.EMAIL_VERIFICATION_TOKEN_SIGNATURE || "signature",
+    AUTH_EXPIRE_TIME: process.env.AUTH_EXPIRE_TIME || "30d",
+    EMAIL_VERIFICATION_EXPIRE_TIME:
+      process.env.EMAIL_VERIFICATION_EXPIRE_TIME || "1h",
+    PASSWORD_RESET_EXPIRE_TIME: process.env.PASSWORD_RESET_EXPIRE_TIME || "15m",
+  },
   emailConfig: {
     SMTP_HOST: process.env.SMTP_HOST,
     SMTP_PORT: process.env.SMTP_PORT,
-    SMTP_USER: process.env.SMTP_SECURE,
-    SMTP_PASSWORD: process.env.SMTP_USERNAME,
+    SMTP_SECURE: process.env.SMTP_SECURE,
+    SMTP_USERNAME: process.env.SMTP_USERNAME,
     SMTP_PASSWORD: process.env.SMTP_PASSWORD,
   },
 };
