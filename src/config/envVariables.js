dd465e4dea902176afbc8524dbea525e4d5bd275
Kerolos <kerolos.saaid@gmail.com>
2024-12-15 20:53:14 +0200

    added SMTP variables

--- src/config/envVariables.js
+++ src/config/envVariables.js
@@ -9,6 +9,13 @@ const envVariables = {
   HASH_SALT: +process.env.HASH_SALT || 8,
   TOKEN_SIGNATURE: process.env.TOKEN_SIGNATURE || "this is a default signature",
   TOKEN_EXPIRE_TIME: process.env.TOKEN_EXPIRE_TIME || "1m",
+  emailConfig: {
+    SMTP_HOST: process.env.SMTP_HOST,
+    SMTP_PORT: process.env.SMTP_PORT,
+    SMTP_USER: process.env.SMTP_SECURE,
+    SMTP_PASSWORD: process.env.SMTP_USERNAME,
+    SMTP_PASSWORD: process.env.SMTP_PASSWORD,
+  },
 };
 
 module.exports = envVariables;
