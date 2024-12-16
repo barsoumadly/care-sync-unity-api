79031037fab34835d0db69973b7a504edd3e841e
Barsoum Adly <barsoumadly@gmail.com>
2024-12-16 14:21:00 +0200

    using cors

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const connectToDatabase = require("./config/database/connectToDatabase");
 const envVariables = require("./config/envVariables");
 const ApiError = require("./utils/ApiError");
@@ -10,6 +11,7 @@ const app = express();
 const port = envVariables.PORT;
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/api/v1", indexRouter);
 
