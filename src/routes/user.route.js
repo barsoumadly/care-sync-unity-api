f3f91612da6c3418e143b6de09fd8f00ec8b6803
kerolos.saaid@gmail.com <kerolos.saaid@gmail.com>
2025-01-31 22:33:58 +0200

    add users route

---
+++ src/routes/user.route.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const { upload } = require("../config/cloudinary");
+const userController = require("../controllers/user.controller");
+
+const router = express.Router();
+
+module.exports = router;
