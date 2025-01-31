dc38750ba991eb7989e7bed8752c5ad2db614b0f
kerolos.saaid@gmail.com <kerolos.saaid@gmail.com>
2025-01-31 23:59:58 +0200

    add update & delete profile photo

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const { upload } = require("../config/cloudinary");
 const userController = require("../controllers/user.controller");
+const auth = require("../middlewares/auth");
 
 const router = express.Router();
+
+router
+  .route("/profile-photo")
+  .put(auth, upload.single("photo"), userController.updateProfilePhoto)
+  .delete(auth, userController.removeProfilePhoto);
 
 module.exports = router;
