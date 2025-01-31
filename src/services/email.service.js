c9d353dfd4def3f89c29d18afb708f0989dd4df0
Kerolos <kerolos.saaid@gmail.com>
2025-01-31 02:16:55 +0200

    use otpObj instead of bare otp

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -7,10 +7,7 @@ module.exports = {
   sendEmailVerificationSuccess: (to) =>
     sendTemplateEmail(to, emailTemplates.emailVerificationSuccess),
   sendPasswordResetRequest: (to, otpObj) =>
-    sendTemplateEmail(to, emailTemplates.passwordResetRequest, {
-      email: to,
-      otpObj,
-    }),
+    sendTemplateEmail(to, emailTemplates.passwordResetRequest, otpObj),
   sendPasswordResetSuccess: (to) =>
     sendTemplateEmail(to, emailTemplates.passwordResetSuccess),
 };
