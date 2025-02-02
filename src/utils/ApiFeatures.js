709afae10768ca12758a547e2e98f6e5d9ed5389
Kerolos <kerolos.saaid@gmail.com>
2025-02-02 22:56:41 +0200

    Add ApiFeatures Class

---
+++ src/utils/ApiFeatures.js
@@ -0,0 +1,71 @@
+class ApiFeatures {
+  constructor(query, queryString) {
+    this.query = query;
+    this.queryString = queryString;
+  }
+
+  // Filter the query based on query string parameters
+  filter() {
+    const queryObj = { ...this.queryString };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach(el => delete queryObj[el]);
+
+    // Advanced filtering: convert query operators like gte, gt, lte, lt to MongoDB format
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+
+    this.query = this.query.find(JSON.parse(queryStr));
+    return this;
+  }
+
+  // Sort the query based on sort parameter
+  sort() {
+    if (this.queryString.sort) {
+      const sortBy = this.queryString.sort.split(',').join(' ');
+      this.query = this.query.sort(sortBy);
+    } else {
+      // Default sorting by createdAt descending
+      this.query = this.query.sort('-createdAt');
+    }
+    return this;
+  }
+
+  // Limit fields selection in the query
+  limitFields() {
+    if (this.queryString.fields) {
+      const fields = this.queryString.fields.split(',').join(' ');
+      this.query = this.query.select(fields);
+    } else {
+      // Exclude __v field by default
+      this.query = this.query.select('-__v');
+    }
+    return this;
+  }
+
+  // Paginate the query results
+  paginate() {
+    const page = parseInt(this.queryString.page, 10) || 1;
+    const limit = parseInt(this.queryString.limit, 10) || 10;
+    const skip = (page - 1) * limit;
+    this.query = this.query.skip(skip).limit(limit);
+    return this;
+  }
+
+  // Utility method for safe JSON stringification to avoid circular structure errors
+  static safeStringify(obj) {
+    const seen = new WeakSet();
+    return JSON.stringify(
+      obj,
+      (key, value) => {
+        if (typeof value === 'object' && value !== null) {
+          if (seen.has(value)) return '[Circular]';
+          seen.add(value);
+        }
+        return value;
+      },
+      2
+    );
+  }
+}
+
+module.exports = ApiFeatures;
