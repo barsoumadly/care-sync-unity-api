e7388b38d57a0e827e7386b3873446cf51f53459
Kerolos <kerolos.saaid@gmail.com>
2025-02-04 00:57:21 +0200

    add appointmentRoute & facilityRoute

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,10 +1,14 @@
 const { Router } = require("express");
 const authRoute = require("./auth.route");
 const userRoute = require("./user.route");
+const facilityRoute = require("./facility.route");
+const appointmentRoute = require("./appointment.route");
 
 const router = Router();
 
 router.use("/auth", authRoute);
 router.use("/users", userRoute);
+router.use("/facilities", facilityRoute);
+router.use("/appointments", appointmentRoute);
 
 module.exports = router;
