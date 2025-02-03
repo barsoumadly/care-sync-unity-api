60ff4c19898060860c73a7b348e6be4bcccc3d48
Kerolos <kerolos.saaid@gmail.com>
2025-02-04 01:46:14 +0200

    init Patient model

--- src/models/Patient.js
+++ src/models/Patient.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-// TODO: Define the schema for the appointment model
+// TODO: Define the schema for the patient model
 const patientSchema = mongoose.Schema();
 
 const Patient = mongoose.model("Patient", patientSchema);
