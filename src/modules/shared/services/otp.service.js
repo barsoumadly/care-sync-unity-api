b1899899d415cc7fb5f75ae75ba3b27d88764583
Kerolos <kerolos.saaid@gmail.com>
2025-02-09 03:36:50 +0200

    feat: extend OTP service to handle email verification

--- src/modules/shared/services/otp.service.js
+++ src/modules/shared/services/otp.service.js
@@ -9,6 +9,16 @@ const generateOTP = () => {
   return Math.floor(min + Math.random() * (max - min)).toString();
 };
 
+const createEmailVerificationOTPObj = async (user) => {
+  const otp = generateOTP();
+  user.emailVerificationOtp = otp;
+  user.emailVerificationOtpExpiry = new Date(
+    Date.now() + ms(otpConfig.PASSWORD_RESET_EXPIRE_TIME)
+  );
+  await user.save();
+  return { otp, expiryTime: ms(otpConfig.PASSWORD_RESET_EXPIRE_TIME) / 60000 };
+};
+
 const createPasswordResetOTPObj = async (user) => {
   const otp = generateOTP();
   user.passwordResetOtp = otp;
@@ -19,7 +29,7 @@ const createPasswordResetOTPObj = async (user) => {
   return { otp, expiryTime: ms(otpConfig.PASSWORD_RESET_EXPIRE_TIME) / 60000 };
 };
 
-const verifyOTP = async (user, otp) => {
+const verifyPasswordResetOTP = async (user, otp) => {
   if (
     !user.passwordResetOtp ||
     user.passwordResetOtp !== otp ||
@@ -30,14 +40,22 @@ const verifyOTP = async (user, otp) => {
   return true;
 };
 
-const clearOTP = async (user) => {
+const clearPasswordResetOTP = async (user) => {
   user.passwordResetOtp = undefined;
   user.passwordResetOtpExpiry = undefined;
   await user.save();
 };
 
+const clearEmailVerificationOTP = async (user) => {
+  user.emailVerificationOtp = undefined;
+  user.emailVerificationOtpExpiry = undefined;
+  await user.save();
+};
+
 module.exports = {
+  createEmailVerificationOTPObj,
   createPasswordResetOTPObj,
-  verifyOTP,
-  clearOTP,
+  verifyPasswordResetOTP,
+  clearPasswordResetOTP,
+  clearEmailVerificationOTP,
 };
