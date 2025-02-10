18e2f89cd28605928fc731dca4d45da50201cd99
Barsoum Adly <barsoumadly@gmail.com>
2025-02-10 13:34:54 +0200

    add verify reset otp route

--- src/modules/auth/auth.route.js
+++ src/modules/auth/auth.route.js
@@ -6,8 +6,12 @@ const router = Router();
 router.post("/register", authController.register);
 router.post("/login", authController.login);
 router.post("/verify-email", authController.verifyEmail);
-router.post("/request-email-verification", authController.requestEmailVerification);
+router.post(
+  "/request-email-verification",
+  authController.requestEmailVerification
+);
 router.post("/request-password-reset", authController.requestPasswordReset);
+router.post("/verify-reset-otp", authController.verifyResetOtp);
 router.post("/reset-password", authController.resetPassword);
 
 module.exports = router;
