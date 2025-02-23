baab559e37ebca36d5b3a07c6edebdbd369bc2b9
Seoudii <92980520+TarekAyman@users.noreply.github.com>
2025-02-23 22:18:09 +0200

    Update index.routes.js

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -4,6 +4,7 @@ const userRoute = require("../modules/user/user.route");
 const facilityRoute = require("../modules/facility/facility.route");
 const appointmentRoute = require("../modules/appointment/appointment.route");
 const chatRoute = require("../modules/chat/chat.route");
+const patientRoute = require("../modules/patient/patient.route");
 
 const router = Router();
 
@@ -12,5 +13,6 @@ router.use("/users", userRoute);
 router.use("/facilities", facilityRoute);
 router.use("/appointments", appointmentRoute);
 router.use("/chats", chatRoute);
+router.use("/patients", patientRoute);
 
 module.exports = router;
