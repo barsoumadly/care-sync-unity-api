02cc89dca768b0367bb1b2d182331e7a4eb7c117
Seoudii <92980520+TarekAyman@users.noreply.github.com>
2025-02-23 22:14:07 +0200

    Create patient.controller.js

---
+++ src/modules/patient/patient.controller.js
@@ -0,0 +1,15 @@
+const Patient = require("../models/Patient");
+const { StatusCodes } = require("http-status-codes");
+const ApiError = require("../utils/ApiError");
+const AsyncHandler = require("../utils/AsyncHandler");
+
+// Get own profile
+const getPatientProfile = AsyncHandler(async (req, res) => {
+  const patient = await Patient.findOne({ userId: req.user._id }).populate("userId", "name email");
+  if (!patient) {
+    throw new ApiError("Patient profile not found", StatusCodes.NOT_FOUND);
+  }
+  res.status(StatusCodes.OK).json({ success: true, data: patient });
+});
+
+module.exports = { getPatientProfile };
