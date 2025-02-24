8fe06f6cb69010c3a7207c35ef101028b8c780c5
Barsoum Adly <barsoumadly@gmail.com>
2025-02-24 17:33:08 +0200

    patient controller

--- src/modules/patient/patient.controller.js
+++ src/modules/patient/patient.controller.js
@@ -1,10 +1,13 @@
-const Patient = require("../models/Patient");
+const Patient = require("../../models/Patient");
 const { StatusCodes } = require("http-status-codes");
-const ApiError = require("../utils/ApiError");
-const AsyncHandler = require("../utils/AsyncHandler");
+const ApiError = require("../../utils/ApiError");
+const AsyncHandler = require("../../utils/AsyncHandler");
 
 const getPatientProfile = AsyncHandler(async (req, res) => {
-  const patient = await Patient.findOne({ userId: req.user._id }).populate("userId", "name email");
+  const patient = await Patient.findOne({ userId: req.user._id }).populate(
+    "userId",
+    "name email"
+  );
   if (!patient) {
     throw new ApiError("Patient profile not found", StatusCodes.NOT_FOUND);
   }
