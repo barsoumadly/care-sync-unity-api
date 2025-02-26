cfc3be9ba671d01c780eb046fa15dc611c8e1bfe
antonrizk3100 <160608215+antonrizk3100@users.noreply.github.com>
2025-02-26 21:24:59 +0200

    doctor controller/router v1
    main endpoints
    still need some changes

---
+++ src/modules/doctor/doctor.route.js
@@ -0,0 +1,13 @@
+const { Router } = require("express");
+const doctorController = require('./doctor.controller'); // Controller Layer
+const auth = require("../../middlewares/auth");
+
+const router = Router();
+router.get('/profile',auth, doctorController.getProfile); // Get doctor's profile
+router.put('/profile',auth, doctorController.updateProfile); // Update profile
+router.get('/patients',auth,  doctorController.listPatients); // List patients under doctor
+router.get('/appointments',auth, doctorController.listAppointments); // List doctor's appointments
+router.get('/appointments/:id',auth, doctorController.getAppointment); // Get specific appointment
+router.put('/appointments/:id',auth, doctorController.updateAppointment); // Update appointment details
+
+module.exports = router;
