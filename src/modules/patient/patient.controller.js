29f4f962b2aead941596b575089e6a5d5743ced2
Barsoum Adly <barsoumadly@gmail.com>
2025-03-07 19:45:07 +0200

    patient controller

--- src/modules/patient/patient.controller.js
+++ src/modules/patient/patient.controller.js
@@ -48,8 +48,16 @@ const listAppointments = AsyncHandler(async (req, res) => {
 });
 
 const bookAppointment = AsyncHandler(async (req, res) => {
-  const { doctorId, clinicId, reasonForVisit, paymentType, scheduledAt } =
-    req.body;
+  const {
+    doctorId,
+    clinicId,
+    reasonForVisit,
+    paymentType,
+    scheduledAt,
+    specialization,
+    price,
+    time,
+  } = req.body;
   const checkAppointment = await Appointment.findOne({
     patientId: req.user._id,
     doctorId,
