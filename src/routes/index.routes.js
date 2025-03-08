6affa773be6b0f9423d225e62347620b69728e7b
Barsoum Adly <barsoumadly@gmail.com>
2025-03-08 10:42:16 +0200

    index routes

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -5,6 +5,7 @@ const facilityRoute = require("../modules/facility/facility.route");
 const appointmentRoute = require("../modules/appointment/appointment.route");
 const chatRoute = require("../modules/chat/chat.route");
 const patientRoute = require("../modules/patient/patient.route");
+const doctorRoute = require("../modules/doctor/doctor.route");
 
 const router = Router();
 
@@ -14,5 +15,6 @@ router.use("/facilities", facilityRoute);
 router.use("/appointments", appointmentRoute);
 router.use("/chats", chatRoute);
 router.use("/patients", patientRoute);
+router.use("/doctors", doctorRoute);
 
 module.exports = router;
