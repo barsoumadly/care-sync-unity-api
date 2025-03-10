af0024e7979b3ff660e2b826c0dc3ff43c229667
Barsoum Adly <barsoumadly@gmail.com>
2025-03-10 16:08:19 +0200

    laboratory routes

--- src/modules/laboratory/laboratory.route.js
+++ src/modules/laboratory/laboratory.route.js
@@ -2,6 +2,8 @@ const { Router } = require("express");
 const {
   getLaboratoryProfile,
   updateLaboratoryProfile,
+  getLaboratoryList,
+  getLaboratory,
 } = require("./laboratory.controller");
 const auth = require("../../middlewares/auth");
 
@@ -9,5 +11,7 @@ const router = Router();
 
 router.get("/profile", auth, getLaboratoryProfile);
 router.put("/profile", auth, updateLaboratoryProfile);
+router.get("/", auth, getLaboratoryList);
+router.get("/:laboratoryId", auth, getLaboratory);
 
 module.exports = router;
