36cb5ac9caae0e3dc791471b6116acea506ab892
Barsoum Adly <barsoumadly@gmail.com>
2025-03-10 18:23:29 +0200

    user controller

--- src/modules/user/user.controller.js
+++ src/modules/user/user.controller.js
@@ -14,14 +14,24 @@ const updateProfilePhoto = AsyncHandler(async (req, res) => {
 });
 
 const removeProfilePhoto = AsyncHandler(async (req, res) => {
-  const user = await userService.removeProfilePhoto(req.user.id);
+  await userService.removeProfilePhoto(req.user.id);
   res.status(StatusCodes.OK).json({
     status: "success",
     message: "Profile photo removed successfully",
   });
 });
 
+const getUser = AsyncHandler(async (req, res) => {
+  const { userId } = req.params;
+  const user = await userService.getUserById(userId);
+  if (!user) {
+    throw new ApiError("No user found with this id", StatusCodes.NOT_FOUND);
+  }
+  res.status(StatusCodes.OK).json({ success: true, data: user });
+});
+
 module.exports = {
   updateProfilePhoto,
   removeProfilePhoto,
+  getUser,
 };
