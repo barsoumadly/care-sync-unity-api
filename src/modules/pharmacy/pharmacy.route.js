dafe348a5a9dee741176ee5d0e1b45394b3a4e21
Barsoum Adly <barsoumadly@gmail.com>
2025-03-10 16:08:34 +0200

    pharmacy routes

--- src/modules/pharmacy/pharmacy.route.js
+++ src/modules/pharmacy/pharmacy.route.js
@@ -2,6 +2,8 @@ const { Router } = require("express");
 const {
   getPharmacyProfile,
   updatePharmacyProfile,
+  getPharmacyList,
+  getPharmacy,
 } = require("./pharmacy.controller");
 const auth = require("../../middlewares/auth");
 
@@ -9,5 +11,7 @@ const router = Router();
 
 router.get("/profile", auth, getPharmacyProfile);
 router.put("/profile", auth, updatePharmacyProfile);
+router.get("/", auth, getPharmacyList);
+router.get("/:pharmacyId", auth, getPharmacy);
 
 module.exports = router;
