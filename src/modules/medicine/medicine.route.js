a9a0f1f127202ef7d877e4e32d604b54f2a9f2d8
Barsoum Adly <barsoumadly@gmail.com>
2025-03-12 11:33:07 +0200

    medicine routes

--- src/modules/medicine/medicine.route.js
+++ src/modules/medicine/medicine.route.js
@@ -1,10 +1,15 @@
 const { Router } = require("express");
-const { getMedicineList, addMedicine } = require("./medicine.controller");
+const {
+  getMedicineList,
+  addMedicine,
+  updateMedicine,
+} = require("./medicine.controller");
 const auth = require("../../middlewares/auth");
 
 const router = Router();
 
 router.get("/", auth, getMedicineList);
 router.post("/", auth, addMedicine);
+router.put("/", auth, updateMedicine);
 
 module.exports = router;
