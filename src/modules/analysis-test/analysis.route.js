d7927ad6cd2295789b62ec02f70c5374570c4369
Barsoum Adly <barsoumadly@gmail.com>
2025-03-12 20:11:32 +0200

    analysis routes

--- src/modules/analysis-test/analysis.route.js
+++ src/modules/analysis-test/analysis.route.js
@@ -4,12 +4,14 @@ const {
   addAnalyisTest,
   updateAnalysisTest,
   deleteAnalysis,
+  getAnalysisListByLaboratoryId,
 } = require("./analysis.controller");
 const auth = require("../../middlewares/auth");
 
 const router = Router();
 
 router.get("/", auth, getAnalysisList);
+router.get("/:id", auth, getAnalysisListByLaboratoryId);
 router.post("/", auth, addAnalyisTest);
 router.put("/", auth, updateAnalysisTest);
 router.delete("/:id", auth, deleteAnalysis);
