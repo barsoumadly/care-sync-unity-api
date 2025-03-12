8450c689b890e58c8edcb5b475b2a82e9ce49784
Kerolos <kerolos.saaid@gmail.com>
2025-03-12 21:46:34 +0200

    feat: add profileCompleted logic to Clinic model

--- src/models/Clinic.js
+++ src/models/Clinic.js
@@ -1,5 +1,18 @@
 const mongoose = require("mongoose");
 
+// Function to check if profile is complete
+const checkProfileComplete = function (clinic) {
+  const hasName = !!clinic.name;
+  const hasPhone = !!clinic.phone;
+  const hasAddress = !!(
+    clinic.address &&
+    clinic.address.street &&
+    clinic.address.city
+  );
+
+  return hasName && hasPhone && hasAddress;
+};
+
 const clinicSchema = mongoose.Schema(
   {
     name: {
@@ -45,5 +58,59 @@ const clinicSchema = mongoose.Schema(
     timestamps: true,
   }
 );
+
+// Pre-save hook
+clinicSchema.pre("save", async function (next) {
+  const isComplete = checkProfileComplete(this);
+  // Update the associated user's profileCompleted field
+  await mongoose.model("User").findByIdAndUpdate(this.adminId, {
+    profileCompleted: isComplete,
+  });
+  next();
+});
+
+// Pre-update hook
+clinicSchema.pre(["updateOne", "findOneAndUpdate"], async function (next) {
+  const update = this.getUpdate();
+  const options = this.getOptions();
+
+  if (update) {
+    try {
+      // For existing documents
+      const doc = await this.model.findOne(this.getQuery());
+      if (doc) {
+        const updatedDoc = {
+          ...doc.toObject(),
+          ...update,
+          ...update.$set
+        };
+        const isComplete = checkProfileComplete(updatedDoc);
+        await mongoose.model("User").findByIdAndUpdate(doc.adminId, {
+          profileCompleted: isComplete
+        });
+      }
+      // For new documents created by upsert
+      else if (options.upsert) {
+        const newDoc = {
+          ...this.getQuery(),
+          ...update,
+          ...update.$set
+        };
+        const isComplete = checkProfileComplete(newDoc);
+        // Get adminId from the query since it's a new document
+        const adminId = this.getQuery().adminId;
+        if (adminId) {
+          await mongoose.model("User").findByIdAndUpdate(adminId, {
+            profileCompleted: isComplete
+          });
+        }
+      }
+    } catch (err) {
+      return next(err);
+    }
+  }
+  next();
+});
+
 const Clinic = mongoose.model("Clinic", clinicSchema);
 module.exports = Clinic;
