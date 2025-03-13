dab58c3c318c66433cf7fc07fbcd729953bcead2
Barsoum Adly <barsoumadly@gmail.com>
2025-03-13 17:32:51 +0200

    medicine order

---
+++ src/models/MedicineOrder.js
@@ -0,0 +1,64 @@
+const mongoose = require("mongoose");
+
+const medicineOrderSchema = new mongoose.Schema({
+  pharmacyId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Pharmacy",
+    required: true,
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  userPhoneNumber: {
+    type: String,
+    required: true,
+  },
+  userAddress: {
+    type: String,
+    required: true,
+  },
+  pharmacyName: {
+    type: String,
+    required: true,
+  },
+  medicines: [
+    {
+      name: {
+        type: String,
+        required: true,
+      },
+      quantity: {
+        type: Number,
+        required: true,
+      },
+      price: {
+        type: Number,
+        required: true,
+      },
+      expirationDate: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+  status: {
+    type: String,
+    enum: ["pending", "on the way", "delivered", "cancelled"],
+    default: "pending",
+  },
+  paymentType: {
+    type: String,
+    enum: ["cash", "credit"],
+    default: "cash",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const MedicineOrder = mongoose.model("MedicineOrder", medicineOrderSchema);
+
+module.exports = MedicineOrder;
