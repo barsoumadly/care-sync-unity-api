43e620241c35402b3fef8e063d3cdf5918cec6d9
Barsoum Adly <barsoumadly@gmail.com>
2025-03-14 16:06:56 +0200

    medicine order routes

--- src/modules/medicine/medicine.route.js
+++ src/modules/medicine/medicine.route.js
@@ -4,12 +4,14 @@ const {
   addMedicine,
   updateMedicine,
   deleteMedicine,
+  getMedicinesByPharmacy,
 } = require("./medicine.controller");
 const auth = require("../../middlewares/auth");
 
 const router = Router();
 
 router.get("/", auth, getMedicineList);
+router.get("/pharmacy/:id", auth, getMedicinesByPharmacy);
 router.post("/", auth, addMedicine);
 router.put("/", auth, updateMedicine);
 router.delete("/:id", auth, deleteMedicine);
