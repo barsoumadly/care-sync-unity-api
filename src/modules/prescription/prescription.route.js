65fade6ba4cfb92e61446279328f71c05826bafd
Barsoum Adly <barsoumadly@gmail.com>
2025-03-28 20:29:49 +0200

    prescription routes

--- src/modules/prescription/prescription.route.js
+++ src/modules/prescription/prescription.route.js
@@ -6,9 +6,11 @@ const auth = require("../../middlewares/auth");
 const {
   createPrescription,
   getPrescriptionsByPatientId,
+  updatePrescription,
 } = require("./prescription.controller");
 
 router.post("/", auth, createPrescription);
+router.put("/:id", auth, updatePrescription);
 router.get("/", auth, getPrescriptionsByPatientId);
 
 module.exports = router;
