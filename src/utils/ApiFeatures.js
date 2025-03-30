fb4375d916901611e8dbbb5e7a3909ac2068faa5
Barsoum Adly <barsoumadly@gmail.com>
2025-03-30 21:18:43 +0200

    api features

--- src/utils/ApiFeatures.js
+++ src/utils/ApiFeatures.js
@@ -7,12 +7,12 @@ class ApiFeatures {
   // Filter the query based on query string parameters
   filter() {
     const queryObj = { ...this.queryString };
-    const excludedFields = ['page', 'sort', 'limit', 'fields'];
-    excludedFields.forEach(el => delete queryObj[el]);
+    const excludedFields = ["page", "sort", "limit", "fields"];
+    excludedFields.forEach((el) => delete queryObj[el]);
 
     // Advanced filtering: convert query operators like gte, gt, lte, lt to MongoDB format
     let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
 
     this.query = this.query.find(JSON.parse(queryStr));
     return this;
@@ -21,11 +21,11 @@ class ApiFeatures {
   // Sort the query based on sort parameter
   sort() {
     if (this.queryString.sort) {
-      const sortBy = this.queryString.sort.split(',').join(' ');
+      const sortBy = this.queryString.sort.split(",").join(" ");
       this.query = this.query.sort(sortBy);
     } else {
       // Default sorting by createdAt descending
-      this.query = this.query.sort('-createdAt');
+      this.query = this.query.sort("-createdAt");
     }
     return this;
   }
@@ -33,11 +33,11 @@ class ApiFeatures {
   // Limit fields selection in the query
   limitFields() {
     if (this.queryString.fields) {
-      const fields = this.queryString.fields.split(',').join(' ');
+      const fields = this.queryString.fields.split(",").join(" ");
       this.query = this.query.select(fields);
     } else {
       // Exclude __v field by default
-      this.query = this.query.select('-__v');
+      this.query = this.query.select("-__v");
     }
     return this;
   }
@@ -45,7 +45,7 @@ class ApiFeatures {
   // Paginate the query results
   paginate() {
     const page = parseInt(this.queryString.page, 10) || 1;
-    const limit = parseInt(this.queryString.limit, 10) || 10;
+    const limit = parseInt(this.queryString.limit, 15) || 15;
     const skip = (page - 1) * limit;
     this.query = this.query.skip(skip).limit(limit);
     return this;
@@ -57,8 +57,8 @@ class ApiFeatures {
     return JSON.stringify(
       obj,
       (key, value) => {
-        if (typeof value === 'object' && value !== null) {
-          if (seen.has(value)) return '[Circular]';
+        if (typeof value === "object" && value !== null) {
+          if (seen.has(value)) return "[Circular]";
           seen.add(value);
         }
         return value;
